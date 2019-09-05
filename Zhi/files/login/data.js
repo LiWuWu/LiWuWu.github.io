eeb106a7edb7844f59ba9f71ae061ab316c8426d
LiWuWu <46261161+LiWuWu@users.noreply.github.com>
2019-09-05 15:31:26 +0800

    11

---
+++ Zhi/files/login/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w],x,_(y,z,A,B,C,q,D,_(),E,_(F,G,H,I,J,_(K,L,M,N),O,null,P,I,Q,I,R,S,T,null,U,V,W,X,Y,Z,ba,V),bb,_(),bc,_(),bd,_(be,[])),bf,_(),bg,_());}; 
+var b="url",c="login.html",d="generationDate",e=new Date(1567668584089.43),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="WebChatChange",o="Page",p="ListPage",q="Login",r="EditorPage",s="TextLength",t="indexpage",u="bol",v="menberpage",w="PhoneTextlength",x="page",y="packageId",z="dd37aea108a74cc98a0a2dd7e077ef9d",A="type",B="Axure:Page",C="name",D="notes",E="style",F="baseStyle",G="627587b6038d43cca051c114ac41ad32",H="pageAlignment",I="near",J="fill",K="fillType",L="solid",M="color",N=0xFFFFFFFF,O="image",P="imageHorizontalAlignment",Q="imageVerticalAlignment",R="imageRepeat",S="auto",T="favicon",U="sketchFactor",V="0",W="colorStyle",X="appliedColor",Y="fontName",Z="Applied Font",ba="borderWidth",bb="adaptiveStyles",bc="interactionMap",bd="diagram",be="objects",bf="masters",bg="objectPaths";
+return _creator();
+})());
